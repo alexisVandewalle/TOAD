90e40a98dddf627d0e929fb4cbc7c391711cb6d8
Alexis Vandewalle <alexis.vandewalle@student.isae-supaero.fr>
2021-02-04 15:57:49 +0100

    first version of TOAD contract

--- contracts/TOAD.sol
+++ contracts/TOAD.sol
@@ -6,8 +6,14 @@ contract TOAD{
 
     /// public variables
     bool public has_been_used = false;
+    bool public is_mpk_available = false;
     uint public N;
     uint public t;
+    address public public_account;
+    uint public round;
+    uint public nb_group_key = 0;
+
+    GroupKeyWithId[] public gpk_list;
 
     ///structures
     struct EncryptedAccount{
@@ -16,7 +22,20 @@ contract TOAD{
         uint256 e_pk;
     }
 
+    struct GroupKeyWithId{
+        uint anonymousId;
+        uint256[2] gpk;
+    }
+
+    ///events
     event GroupCreation(EncryptedAccount[] group, uint threshold, string label);
+    event PublicKey(uint256[2] public_key, uint anonymous_id);
+    event Share(uint256[] shares, uint round);
+    event GroupKey(uint256[2] gpk, uint anonymous_id, uint round);
+    event MasterGroupKeyAvailable();
+    event ShareForDec(address sender, uint id, uint256[4] share, uint256[2] proof);
+    event NewMessage(address sender, uint round, bytes file_hash, uint256[4] c1, uint256[4] c2);
+    event GenerateNewKeys(uint round);
 
     function groupCreation(
         EncryptedAccount[] memory _group, 
@@ -24,13 +43,59 @@ contract TOAD{
         string memory _label
         ) public {
         
-        require(!has_been_used);
+        require(!has_been_used,'group already created');
         has_been_used = true;
 
         N = _group.length;
-        require(_threshold < N);
+        require(_threshold < N,'threshold must satisfy threshold<N');
         t = _threshold;
+        public_account = msg.sender;
+        round = 0;
 
         emit GroupCreation(_group, _threshold, _label);
     }
+
+    function publish_pk(uint256[2] memory _public_key, uint _anonymous_id) public{
+        require(msg.sender == public_account, 'only user who have access to the public account can call this function');
+        emit PublicKey(_public_key, _anonymous_id);
+    }
+
+    function publish_share(uint256[] memory _shares) public{
+        require(msg.sender == public_account, 'only user who have access to the public account can call this function');
+        emit Share(_shares, round);
+    }
+
+    function register_group_key(uint256[2] memory _gpk, uint _anonymous_id, uint _round)public{
+        require(msg.sender == public_account,'only user who have access to the public account can call this function');
+        
+        emit GroupKey(_gpk, _anonymous_id, _round); 
+
+        if(_round == 0){
+            nb_group_key +=1;
+            gpk_list.push(GroupKeyWithId(_anonymous_id, _gpk));
+            if(nb_group_key == t +1){
+                is_mpk_available = true;
+                emit MasterGroupKeyAvailable();
+            }
+        }
+    }
+
+    function get_group_keys()public view returns(GroupKeyWithId[] memory){
+        return gpk_list;
+    }
+
+    function send_msg(bytes memory file_hash, uint256[4] memory c1, uint256[4] memory c2) public{
+        require(is_mpk_available, 'master key is not available');
+        emit NewMessage(msg.sender,round, file_hash, c1, c2);
+        if (round>0){
+            emit GenerateNewKeys(round);
+        }
+        round = round + 1;
+    }
+
+    function share_for_dec(uint _round, uint256[4] memory _share, uint256[2] memory _proof) public{
+        require(is_mpk_available, 'no message available');
+        emit ShareForDec(msg.sender, _round, _share, _proof);
+    }
+
 }
