7454f9d7cab128dd1056fbfdde97f7969dcccfb2
Alexis Vandewalle <alexis.vandewalle@student.isae-supaero.fr>
2021-03-22 12:06:46 +0100

    solve conflict

--- client/webapp/decorators.py
+++ client/webapp/decorators.py
@@ -9,6 +9,9 @@ def gas_cost(method):
         gas_used = self.w3.eth.getTransactionReceipt(txn_hash)['gasUsed']
         function_name = method.__name__
         with open("./gas_cost/gas_cost.csv", "a") as f:
-            f.write(function_name+","+str(gas_used)+","+self.private_key[0:6]+"\n")
+            f.write(function_name+","
+                +str(gas_used)+","
+                +self.private_key[0:6]+","
+                +time.time()+"\n")
 
     return wrapper
