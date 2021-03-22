96d8cccaa5c90b2143a8356fa28248407c75a8a7
Alexis Vandewalle <alexis.vandewalle@student.isae-supaero.fr>
2021-03-22 11:55:36 +0100

    change color, add cumul graph

--- gas_cost_plot.py
+++ gas_cost_plot.py
@@ -28,7 +28,7 @@ for user in group:
 
 
 # stats - min - mean - max init phase
-init = df.drop(columns=['send_file','send_share'])
+init = df[(df['function_name']!='send_file') & (df['function_name']!='send_share')]
 init_stats = init.groupby(["round", 'function_name']).mean()
 init_stats.rename(columns={"gas_cost":"gas_cost_mean"}, inplace=True)
 init_stats['gas_cost_max'] = init.groupby(["round", 'function_name']).max()['gas_cost']
@@ -38,14 +38,38 @@ init_stats['gas_cost_min'] = init.groupby(["round", 'function_name']).min()['gas
 
 _, ax = plt.subplots(2,2)
 init_stats.loc[0].gas_cost_max.plot(kind='bar', label="gas cost max", color="none",edgecolor="red", ax=ax[0,0],rot=0)
-init_stats.loc[0].gas_cost_mean.plot(kind='bar', label="gas cost mean", color='green', ax=ax[0,0],rot=0)
+init_stats.loc[0].gas_cost_mean.plot(kind='bar', label="gas cost mean", color='#BADA55', ax=ax[0,0],rot=0)
 init_stats.loc[0].gas_cost_min.plot(kind='bar', label="gas cost min", color="none", edgecolor="blue", ax=ax[0,0],rot=0)
-ax[0,0].set_title("initialisation cost - first file")
+ax[0,0].set_title("Initialisation cost by user- first file")
 
 init_stats.loc[1].gas_cost_max.plot(kind='bar', label="gas cost max", color="none",edgecolor="red", ax=ax[1,0],rot=0)
-init_stats.loc[1].gas_cost_mean.plot(kind='bar', label="gas cost mean", color='green', ax=ax[1,0],rot=0)
+init_stats.loc[1].gas_cost_mean.plot(kind='bar', label="gas cost mean", color='#BADA55', ax=ax[1,0],rot=0)
 init_stats.loc[1].gas_cost_min.plot(kind='bar', label="gas cost min", color="none", edgecolor="blue", ax=ax[1,0],rot=0)
-ax[1,0].set_title("initialisation cost - next file")
+ax[1,0].set_title("Initialisation cost by user - next file")
+
+# stats encryption - decryption
+encrypt = df[(df['function_name']=='send_file') | (df['function_name']=='send_share')].drop(columns=['round'])
+encrypt_stats = encrypt.groupby('function_name').mean()
+encrypt_stats.rename(columns={"gas_cost":"gas_cost_mean"}, inplace=True)
+encrypt_stats['gas_cost_max'] = encrypt.groupby('function_name').max()['gas_cost']
+encrypt_stats['gas_cost_min'] = encrypt.groupby('function_name').min()['gas_cost']
+
+encrypt_stats.gas_cost_max.plot(kind='bar', label="gas cost max", color="none",edgecolor="red", ax=ax[0,1],rot=0)
+encrypt_stats.gas_cost_mean.plot(kind='bar', label="gas cost mean", color='#BADA55', ax=ax[0,1],rot=0)
+encrypt_stats.gas_cost_min.plot(kind='bar', label="gas cost min", color="none", edgecolor="blue", ax=ax[0,1],rot=0)
+ax[0,1].set_title("Encryption-decryption cost by user")
+
+# stats gas cumul per round
+cumul = df[(df['function_name']!='send_file') & (df['function_name']!="group_creation")]
+cumul = cumul.groupby(["round","function_name"]).mean()
+cumul = cumul.groupby('round').sum()
+cumul = cumul.cumsum()
+cumul.rename(columns={"gas_cost": "cumuled_gas_cost_per_file"}, inplace=True)
+
+cumul.cumuled_gas_cost_per_file.plot(kind='bar', color='#BADA55', ax=ax[1,1], rot=0)
+ax[1,1].set_title('Cumuled gas cost per file (without send_file and group_creation step)')
+
 ax[0,0].legend()
 ax[1,0].legend()
+ax[0,1].legend()
 plt.show()
